66095fa9905b0a50f6194504f4f3b1e2ebdcf86c
Dilara Nur Dönmez <116759067+dilaradonmezz@users.noreply.github.com>
2023-01-13 20:30:44 +0300

    Delete maasHesaplama.java

--- maasHesaplama.java
+++
@@ -1,8 +0,0 @@
-public class maasHesaplama {
-    public static void main(String[] args){
-
-        Employee e1= new Employee("John", 2000.0, 45, 1985);
-        e1.toString();
-        e1.run();
-    }
-}
